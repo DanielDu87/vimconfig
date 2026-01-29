2f692c94e9539541972e72e4c95f0245c34be123
Daniel Du <danieldyx87@qq.com>
2026-01-29 15:58:43 +0800

    fix: 修复 mini.comment 插件初始化错误

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,12 +5,22 @@ return {
     opts = {
       options = {
         custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+          -- 安全调用，以防 ts_context_commentstring 不可用
+          local ok, ts_comment = pcall(require, "ts_context_commentstring.internal")
+          if ok then
+            return ts_comment.calculate_commentstring() or vim.bo.commentstring
+          end
+          return vim.bo.commentstring
         end,
       },
     },
+    config = function(_, opts)
+      require("mini.comment").setup(opts)
+    end,
     keys = {
-      { "<leader>c/", function()
+      {
+        "<leader>c/",
+        function()
           require("mini.comment").toggle()
         end,
         mode = { "n", "v" },
